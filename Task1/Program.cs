8e0a48f5c1a7266d4444a6aa7c8034ab860131c0
Tina Shumilova <shutiva@gmail.com>
2021-11-02 15:41:22 +0200

    commit Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+Создать на его основе масив B, отбрасывая те элементы, которые
+
+1. нарушают порядок возрастания
+2. больше среднего арифметического элементов A
+3. чётные*/
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("II. Создаём целочисленный массив A из натуральных двузначных чисел.");
+Console.WriteLine();
+Console.Write("Массив А : ");
+
+int length = 10;
+int[] A = new int[length];
+int index = 0;
+while (index < length)
+{
+    A[index] = new Random().Next(10, 100);
+    Console.Write($"{A[index]} ");
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Создаём на его основе масив B, отбрасывая те элементы, которые");
+Console.WriteLine();
+Console.WriteLine("1. нарушают порядок возрастания,");
+Console.WriteLine();
+
+index = 1;
+int temp = A[0];
+Console.Write($"Массив В.1 : {temp} ");
+while(index < length)
+{
+    if(A[index] > temp)
+    {
+        Console.Write($"{A[index]} ");
+        temp = A[index];
+    }
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("2. больше среднего арифметического элементов A,");
+Console.WriteLine();
+
+double midA = 0;
+index = 0;
+while(index < length)
+{
+    midA = midA + A[index];
+    index++;
+}
+midA = midA / length;
+Console.WriteLine($"Среднее арифметическое элементов массива А = {midA}");
+Console.WriteLine();
+
+index = 0;
+Console.Write("Массив В.2 : ");
+while(index < length)
+{
+    if(A[index] < midA)
+    {
+        Console.Write($"{A[index]} ");
+    }
+    index++;
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("3. чётные.");
+Console.WriteLine();
+
+index = 0;
+Console.Write("Массив В.3 : ");
+while(index < length)
+{
+    if(A[index]%2 == 0)
+    {
+        Console.Write($"{A[index]} ");
+    }
+    index++;
+}
+Console.WriteLine();
+Console.WriteLine();
