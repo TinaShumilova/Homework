a7354f5c13117115dfc5d6dac2800fc0d469206d
Tina Shumilova <shutiva@gmail.com>
2021-11-03 19:52:48 +0200

    последнее редактирование

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,88 +5,6 @@
 2. больше среднего арифметического элементов A
 3. чётные*/
 
-/*Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("II. Создаём целочисленный массив A из натуральных двузначных чисел.");
-Console.WriteLine();
-Console.Write("Массив А : ");
-
-int length = 10;
-int[] A = new int[length];
-int index = 0;
-while (index < length)
-{
-    A[index] = new Random().Next(10, 100);
-    Console.Write($"{A[index]} ");
-    index++;
-}
-Console.WriteLine();
-
-Console.WriteLine();
-Console.WriteLine("Создаём на его основе масив B, отбрасывая те элементы, которые");
-Console.WriteLine();
-Console.WriteLine("1. нарушают порядок возрастания,");
-Console.WriteLine();
-
-index = 1;
-int temp = A[0];
-Console.Write($"Массив В.1 : {temp} ");
-while(index < length)
-{
-    if(A[index] > temp)
-    {
-        Console.Write($"{A[index]} ");
-        temp = A[index];
-    }
-    index++;
-}
-Console.WriteLine();
-
-Console.WriteLine();
-Console.WriteLine("2. больше среднего арифметического элементов A,");
-Console.WriteLine();
-
-double midA = 0;
-index = 0;
-while(index < length)
-{
-    midA = midA + A[index];
-    index++;
-}
-midA = midA / length;
-Console.WriteLine($"Среднее арифметическое элементов массива А = {midA}");
-Console.WriteLine();
-
-index = 0;
-Console.Write("Массив В.2 : ");
-while(index < length)
-{
-    if(A[index] < midA)
-    {
-        Console.Write($"{A[index]} ");
-    }
-    index++;
-}
-Console.WriteLine();
-
-Console.WriteLine();
-Console.WriteLine("3. чётные.");
-Console.WriteLine();
-
-index = 0;
-Console.Write("Массив В.3 : ");
-while(index < length)
-{
-    if(A[index]%2 == 0)
-    {
-        Console.Write($"{A[index]} ");
-    }
-    index++;
-}
-Console.WriteLine();
-Console.WriteLine();
-*/
-
 int[] CreateArrayA(int[] Array, int min, int max)
 {
     int length = Array.Length;
@@ -167,7 +85,7 @@ int[] CreateArrayB3(int[] array)
     Console.Write("Массив В.3 : ");
     while(inde < lent)
     {
-        if(array[inde]%2 == 0)
+        if(array[inde]%2 != 0)
         {
             ArrB[index] = array[inde];
             Console.Write($"{ArrB[index]} ");
@@ -178,10 +96,8 @@ int[] CreateArrayB3(int[] array)
     return ArrB;
 }
 
-
 Console.WriteLine();
-Console.WriteLine("II. Создаём целочисленный массив A из натуральных двузначных чисел.");
-Console.WriteLine();
+Console.WriteLine("Создаём целочисленный массив A из натуральных двузначных чисел.");
 Console.Write("Массив А : ");
 
 int length = 10;
@@ -190,19 +106,20 @@ int[] ArrayA = new int[length]; //Массив А :
 CreateArrayA(ArrayA, 10, 100);
 Console.WriteLine();
 
-//Создаём на его основе масив B, отбрасывая те элементы, которые
-//1. нарушают порядок возрастания,
+Console.WriteLine("Создаём на его основе масив B, отбрасывая те элементы, которые");
+Console.WriteLine("1. нарушают порядок возрастания,");
+
 
 int[] ArrayB1 = CreateArrayB1(ArrayA); // Массив В.1
 Console.WriteLine();
 
-//2. больше среднего арифметического элементов A,
+Console.WriteLine("2. больше среднего арифметического элементов A,");
 
 Console.WriteLine($"Среднее арифметическое элементов массива А = {midA(ArrayA)}");
+
 int[] ArrayB2 = CreateArrayB2(ArrayA); // Массив В2
 
-//3. чётные.
+Console.WriteLine("3. чётные.");
 
-int[] ArrayB3 = CreateArrayB3(ArrayA);
+int[] ArrayB3 = CreateArrayB3(ArrayA); // Массив В3
 Console.WriteLine();
-
